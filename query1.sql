3cffb44912777bc7c462624b9eac62498ff50ede
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-10-04 20:18:24 -0700

    Make TrackId Unique to enforce 1-0/1 relation

--- query1.sql
+++ query1.sql
@@ -3,7 +3,7 @@
 -- There cannot be a video without a track, and each track can have either no video or just one. 
 CREATE TABLE "music_video" (
 	"MusicVideoId"	INTEGER NOT NULL UNIQUE,
-	"TrackId"	INTEGER NOT NULL,
+	"TrackId"	INTEGER NOT NULL UNIQUE,
 	"Director"	TEXT NOT NULL,
 	PRIMARY KEY("MusicVideoId" AUTOINCREMENT),
 	FOREIGN KEY("TrackId") REFERENCES "tracks"("TrackId")
